4ffa2fe7992757daf21540109e51f15bcc5c7881
Addilete <165387997+adillette@users.noreply.github.com>
2026-04-20 11:55:23 +0900

    feat: 주문 생성 기능 추가함

--- src/main/java/com/credential/shop/service/OrderService.java
+++ src/main/java/com/credential/shop/service/OrderService.java
@@ -1,5 +1,27 @@
 package com.credential.shop.service;
 
-public class OrderService {
+import org.springframework.stereotype.Service;
 
+import com.credential.shop.domain.Order;
+import com.credential.shop.dto.request.OrderCreateRequest;
+import com.credential.shop.dto.response.OrderCreateResponse;
+import com.credential.shop.repository.OrderRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+  private OrderRepository orderRepository;
+
+  public OrderCreateResponse createOrder(OrderCreateRequest request){
+    Order order = new Order(
+      request.getProductId(),
+      request.getQuantity(),
+      request.getTotalAmount());
+
+    orderRepository.save(order);
+
+    return new OrderCreateResponse(order.getOrderId(), order.getTotalAmount(),order.getStatus().name());
+  }
 }
