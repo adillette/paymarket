da00474d5adfd951adb3e6d59689353bc09ba355
Addilete <165387997+adillette@users.noreply.github.com>
2026-04-20 11:55:13 +0900

    feat: 주문 생성 기능 추가함

--- src/main/java/com/credential/shop/controller/OrderController.java
+++ src/main/java/com/credential/shop/controller/OrderController.java
@@ -1,5 +1,32 @@
 package com.credential.shop.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.credential.shop.dto.request.OrderCreateRequest;
+import com.credential.shop.dto.response.OrderCreateResponse;
+import com.credential.shop.service.OrderService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
 public class OrderController {
 
+  private final OrderService orderService;
+
+  @PostMapping("/create")
+  public ResponseEntity<OrderCreateResponse> createOrder(@RequestBody OrderCreateRequest request) {
+      
+    OrderCreateResponse response=orderService.createOrder(request);
+      
+    return ResponseEntity.ok(response);
+  }
+  
 }
