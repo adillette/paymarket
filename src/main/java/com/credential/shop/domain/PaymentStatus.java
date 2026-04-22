7b614727c6ba75e2554a9b495340ce411c6ad7cb
Addilete <165387997+adillette@users.noreply.github.com>
2026-04-22 19:05:22 +0900

    fix: redis에 상태 추가하기 위해서 PaymentStatus 내용 추가함

--- src/main/java/com/credential/shop/domain/PaymentStatus.java
+++ src/main/java/com/credential/shop/domain/PaymentStatus.java
@@ -1,5 +1,11 @@
 package com.credential.shop.domain;
 
 public enum PaymentStatus {
-READY, PAID, FAILED
+READY, 
+PROCESSING,//redis 상태 처리흐름용 toss api 호출중
+PAID, //db 저장까지 완료
+FAILED,
+RETRYING, //재시도중(redis 상태 처리흐름용)
+UNKNOWN, //응답 유실 재시도 필요(redis 상태 처리흐름용)
+
 }
