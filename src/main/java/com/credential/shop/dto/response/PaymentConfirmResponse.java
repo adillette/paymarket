5761a9808aef56165c3571bd265b95eafa2e9f55
Addilete <165387997+adillette@users.noreply.github.com>
2026-04-22 19:11:09 +0900

    fix: 테스트용 필드 삭제

--- src/main/java/com/credential/shop/dto/response/PaymentConfirmResponse.java
+++ src/main/java/com/credential/shop/dto/response/PaymentConfirmResponse.java
@@ -1,17 +1,19 @@
 package com.credential.shop.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class PaymentConfirmResponse {
-  private int result;
-  private String message;
   
   private String paymentKey;
   private String orderId;
   private String status;
+  @JsonProperty
   private Integer totalAmount;
+  @JsonProperty
   private String approvedAt;
 }
