1c29fbb9e1e63e5ad4e77c8bbaf6e519afc8a3ef
Addilete <165387997+adillette@users.noreply.github.com>
2026-04-23 15:51:37 +0900

    fix:toss 에서 보내는 이름이랑 같아서 jsonproperty 필요없음

--- src/main/java/com/credential/shop/dto/response/PaymentConfirmResponse.java
+++ src/main/java/com/credential/shop/dto/response/PaymentConfirmResponse.java
@@ -2,18 +2,22 @@ package com.credential.shop.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor 
+@AllArgsConstructor 
 public class PaymentConfirmResponse {
   
   private String paymentKey;
   private String orderId;
   private String status;
-  @JsonProperty
+  
   private Integer totalAmount;
-  @JsonProperty
+  
   private String approvedAt;
 }
